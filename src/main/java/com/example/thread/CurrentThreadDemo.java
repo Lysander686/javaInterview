368a23ecaed98dff820cae2bd4d7111e5f9845f3
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-02-04 22:15:47 +0800

    8-1 进程和线程的区别

---
+++ src/main/java/com/example/thread/CurrentThreadDemo.java
@@ -0,0 +1,8 @@
+package com.example.thread;
+
+public class CurrentThreadDemo {
+    public static void main(String[] args) {
+        System.out.println("current Thread: " + Thread.currentThread().getName());
+
+    }
+}
