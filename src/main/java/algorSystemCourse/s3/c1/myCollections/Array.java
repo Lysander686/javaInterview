140113cbcb843b725933f98c4e710dac8a3274f0
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-02-05 23:58:12 +0800

    1-2 堆的基础表示

---
+++ src/main/java/algorSystemCourse/s3/c1/myCollections/Array.java
@@ -0,0 +1,11 @@
+package algorSystemCourse.s3.c1.myCollections;
+
+import java.util.ArrayList;
+
+public class Array extends ArrayList {
+//    private int size;
+//    public void swap(int i, int j){
+//        if (i<0 || i>=size )
+//    }
+
+}
