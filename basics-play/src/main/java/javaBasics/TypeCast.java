ce43be6eaa914b402d284d6c875045352f7d95f8
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-04-09 02:14:34 +0800

    Future - promise of java

---
+++ basics-play/src/main/java/javaBasics/TypeCast.java
@@ -0,0 +1,13 @@
+package javaBasics;
+
+import javaBasics.extendz.Student;
+
+public class TypeCast {
+    public static void main(String[] args) {
+        String name = "Zed";
+        Object name2 = name;
+
+        Student student = new Student();
+        Object student2 = student;
+    }
+}
