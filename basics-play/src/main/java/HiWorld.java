8651a44ac950fa77f52eb456f0ee27c1b7310782
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-06-21 11:30:49 +0800

    generate random num

--- basics-play/src/main/java/HiWorld.java
+++ basics-play/src/main/java/HiWorld.java
@@ -1,4 +1,11 @@
+import java.util.Random;
+
 public class HiWorld {
     public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < 1; i++) {
+            int res = random.nextInt(3) + 1;
+            System.out.println("random choice: " + res);
+        }
     }
 }
